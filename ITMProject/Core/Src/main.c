33edf73fdb5f8ad9b00968815a9e14a999aaac5a
Hayden <haydenhiggins8@gmail.com>
2023-11-10 19:45:43 -0600

    next time look into the adc init functions to find out how to read data

--- ITMProject/Core/Src/main.c
+++ ITMProject/Core/Src/main.c
@@ -239,7 +239,7 @@ static void MX_ADC1_Init(void)
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.NbrOfConversion = 2;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();
