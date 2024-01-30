decc418e4bffdcfc2b69248a8a16660fb565988a
unknown <jessehaynie02@gmail.com>
2024-01-30 16:07:56 -0600

    Disabled Autoretransmission and AutoWakeUp

--- ITMProject/Core/Src/main.c
+++ ITMProject/Core/Src/main.c
@@ -530,8 +530,8 @@ static void MX_CAN_Init(void)
   hcan.Init.TimeSeg2 = CAN_BS2_1TQ;
   hcan.Init.TimeTriggeredMode = DISABLE;
   hcan.Init.AutoBusOff = ENABLE;
-  hcan.Init.AutoWakeUp = ENABLE;
-  hcan.Init.AutoRetransmission = ENABLE;
+  hcan.Init.AutoWakeUp = DISABLE;
+  hcan.Init.AutoRetransmission = DISABLE;
   hcan.Init.ReceiveFifoLocked = DISABLE;
   hcan.Init.TransmitFifoPriority = DISABLE;
   if (HAL_CAN_Init(&hcan) != HAL_OK)
